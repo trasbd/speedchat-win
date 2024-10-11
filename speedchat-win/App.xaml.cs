9f9510fd86002aa536bc7759e6478b96a6b23f42
Thomas Robert <trobert@hydromat.com>
2024-10-11 15:54:02 -0500

    buttons

--- speedchat-win/App.xaml.cs
+++ speedchat-win/App.xaml.cs
@@ -6,7 +6,20 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MainPage();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 140;
+            const int newHeight = 90;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
         }
     }
 }
--- speedchat-win/MainPage.xaml.cs
+++ speedchat-win/MainPage.xaml.cs
@@ -12,13 +12,19 @@
         private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
-
+            /*
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+            */
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 
